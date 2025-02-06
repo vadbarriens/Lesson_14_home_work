4f16b7ef3b60253907da720adf27cb65f8287305
Vadim Burin <burinvad@mail.ru>
2025-02-06 22:37:29 +0300

    add classes

---
+++ src/classes.py
@@ -0,0 +1,29 @@
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self. quantity = quantity
+
+
+class Category:
+    name: str
+    description: str
+    products: list
+
+    product_count = 0
+    category_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
