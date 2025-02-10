30442d4a71dcaa3f82ef4e11f3f63098ec866337
Vadim Burin <burinvad@mail.ru>
2025-02-10 22:15:29 +0300

    add price setter

--- src/products.py
+++ src/products.py
@@ -9,16 +9,31 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        else:
+            if new_price < self.__price:
+                user_response = input('цена товара понижается, если согласны нажмите "y" (yes): ')
+                if user_response == 'y':
+                    self.__price = new_price
 
     @classmethod
     def new_product(cls, dict_products):
         name = dict_products['name']
         description = dict_products['description']
-        price = dict_products['price']
         quantity = dict_products['quantity']
+        price = dict_products['price']
         return cls(name, description, price, quantity)
 
 
