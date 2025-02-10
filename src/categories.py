591507eb26db9a5c04118d273ca0f9e74957177d
Vadim Burin <burinvad@mail.ru>
2025-02-10 21:34:13 +0300

    add new methods in classes

--- src/categories.py
+++ src/categories.py
@@ -1,3 +1,5 @@
+from src.products import Product
+
 class Category:
     """Класс, подсчитывающий количество товаров и количество категорий"""
 
@@ -11,7 +13,20 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        list_product = ''
+        for prod in self.__products:
+            list_product = f'{prod.name}, {prod.price} руб. Остаток: {prod.quantity} шт.'
+        return list_product
--- src/products.py
+++ src/products.py
@@ -11,3 +11,15 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, dict_products):
+        name = dict_products['name']
+        description = dict_products['description']
+        price = dict_products['price']
+        quantity = dict_products['quantity']
+        return cls(name, description, price, quantity)
+
+
+
