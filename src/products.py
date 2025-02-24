fa3c9b55accd056574d0dc5f7c1b360064b09633
Vadim Burin <burinvad@mail.ru>
2025-02-24 20:15:27 +0300

    add mixin

--- src/products.py
+++ src/products.py
@@ -1,7 +1,8 @@
 from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
 
-class Product(BaseProduct):
+class Product(BaseProduct, PrintMixin):
     """Класс, описывающий товары, цену и наличие"""
 
     name: str
@@ -17,6 +18,7 @@ class Product(BaseProduct):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         """
