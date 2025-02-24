18d26a56a415754bae916f675f1d03f0238653dc
Vadim Burin <burinvad@mail.ru>
2025-02-24 20:37:37 +0300

    bugs fixes

--- src/print_mixin.py
+++ src/print_mixin.py
@@ -4,4 +4,4 @@ class PrintMixin:
         print(repr(self))
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})'
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
--- tests/test_print_mixin.py
+++ tests/test_print_mixin.py
@@ -1,6 +1,10 @@
 from src.products import Product
 
+
 def test_print_mixin(capsys):
-    Product("Samsung Galaxy S23 Ultra","256GB, Серый цвет, 200MP камера",180000.0,5,)
+    Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     message = capsys.readouterr()
-    assert message.out.strip() == 'Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)'
+    assert (
+        message.out.strip()
+        == "Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)"
+    )
