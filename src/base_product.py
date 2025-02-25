1781d046e604e640f0be941a27239207ae0cfbca
Vadim Burin <burinvad@mail.ru>
2025-02-25 19:09:20 +0300

    bugs fixes

--- src/base_product.py
+++ src/base_product.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 class BaseProduct(ABC):
 
+    @abstractmethod
     def __init__(self, name, description, price, quantity):
         """
         Абстрактный метод класса Product
